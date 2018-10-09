0f47cf02c0474a81744fb6dc86a6f39c4b03a9e9
minersail <jkwoo1234@gmail.com>
2018-10-09 16:35:15 -0400

    added support for multi-select

--- content.js
+++ content.js
@@ -1,169 +1,235 @@
+class ImageSelection {
+	constructor(p1x, p1y, p2x, p2y)
+	{
+		this.p1 = {x: p1x, y: p1y};
+		this.p2 = {x: p2x, y: p2y};
+	}
+
+	getTopLeft()
+	{
+		return {top: Math.min(this.p1.y, this.p2.y), 
+				left: Math.min(this.p1.x, this.p2.x)};
+	}
+	
+	getDimensions()
+	{
+		return {width: Math.abs(this.p1.x - this.p2.x),
+				height: Math.abs(this.p1.y - this.p2.y)};
+	}
+
+	setOrigin(x, y)
+	{
+		this.p1.x = x;
+		this.p1.y = y;
+	}
+
+	setPoint2(x, y)
+	{
+		this.p2.x = x;
+		this.p2.y = y;
+	}
+}
+
 class ImageData {
 	constructor(image)
 	{
 		this.url = image.src;
-		this.natWidth = image.naturalWidth;
-		this.natHeight = image.naturalHeight;
-		this.width = image.width;
-		this.height = image.height;
+		this.scale = {x: (image.width / image.naturalWidth), 
+					  y: (image.height / image.naturalHeight)};
 
-		this.x1 = 10;
-		this.x2 = 20;
-		this.y1 = 10;
-		this.y2 = 20;
+		this.selections = [new ImageSelection(10, 10, 20, 20)];
 	}
 	
-	setOrigin(x, y)
+	getSelection()
 	{
-		this.x1 = x;
-		this.x2 = x;
-		this.y1 = y;
-		this.y2 = y;
-	}
-	
-	setDimensions(x, y)
-	{
-		this.x2 = x;
-		this.y2 = y;
+		return this.selections[this.selections.length - 1];
 	}
 
-	getTopLeft(scaled=false)
+	addSelection()
 	{
-		if (scaled)
-			return {top: Math.min(this.y1, this.y2) / this.getScale().y, 
-					left: Math.min(this.x1, this.x2) / this.getScale().x};
-		else
-			return {top: Math.min(this.y1, this.y2), 
-					left: Math.min(this.x1, this.x2)};
-	}
-	
-	getDimensions(scaled=false)
-	{
-		if (scaled)
-			return {width: Math.abs(this.x1 - this.x2) / this.getScale().x,
-					height: Math.abs(this.y1 - this.y2) / this.getScale().y};
-		else
-			return {width: Math.abs(this.x1 - this.x2),
-					height: Math.abs(this.y1 - this.y2)};
+		this.selections.push(new ImageSelection(10, 10, 20, 20));
 	}
 
-	getScale()
+	removeSelection()
 	{
-		return {x: this.width / this.natWidth,
-				y: this.height / this.natHeight};
-	}
-	
-	isComplete()
-	{
-		if (this.url.substr(0, 5) === "data:") // Disallow png encodings to be downloaded
-		{
-			alert("This image has bad encoding. Please use a different image.");
-			return false;
-		}
-
-		return (this.x1 !== undefined) &&
-			(this.getDimensions().width != 0) &&
-			(this.getDimensions().height != 0);
+		this.selections.pop();
 	}
 	
 	toString()
 	{
-		return this.url.split("/").pop().split("?")[0] + " " +
-			this.getTopLeft(true).top + " " +
-			this.getTopLeft(true).left + " " +
-			this.getDimensions(true).width + " " +
-			this.getDimensions(true).height;
+		let str = this.url.split("/").pop().split("?")[0] + " ";
+
+		for (let i = 0; i < this.selections.length; i++)
+		{
+			str = str + 
+				(this.selections[i].getTopLeft().top / this.scale.y) + " " +
+				(this.selections[i].getTopLeft().left / this.scale.x) + " " +
+				(this.selections[i].getDimensions().width / this.scale.x) + " " +
+				(this.selections[i].getDimensions().height / this.scale.y) + "\n";
+		}
+
+		return str;
 	}
 }
 
 let images = [];
 let final_images = [];
-let lastImageData = null;
+let currData = null;
+let currImg = null; // only used once to highlight image on button press - kinda hacky tbh
 let highlighting = false;
+
+let MAX_SELECT = 10;
+
+// Bitmask set up (0/1) - display none/block, border dashed/solid, color red/green
+let NONE = 0b000;
+let ACTIVE = 0b100;
+let SELECTED = 0b110;
+let SAVED = 0b111;
 
 $(document).mousemove(function(e)
 {
 	let img = $("img:hover");
 	if (img.length == 0) return; // We are not over an image
 
-	let url = img.get(0).src;		
-	let image = getImage(final_images, url);
+	let url = img.get(0).src;
+	let imageData = getImageData(final_images, url);
 
-	if (image === null) // ImageData was not finalized
-	{			
-		image = getImage(images, url) || images[images.push(new ImageData(img.get(0))) - 1];
+	if (imageData === null)
+	{
+		imageData = getImageData(images, url) || images[images.push(new ImageData(img.get(0))) - 1];
 
-		if (e.shiftKey)
-		{
+		if (e.shiftKey && imageData.selections.length <= MAX_SELECT)
+		{			
+			if (url.substr(0, 5) === "data:") // Disallow png encodings to be downloaded
+			{
+				alert("This image has bad encoding. Please use a different image.");
+				return;
+			}
+
+			// posX and posY are cursor's position relative to image
 			let posX = e.pageX - img.offset().left;
 			let posY = e.pageY - img.offset().top;
 
 			if (!highlighting)
 			{
-				image.setOrigin(posX, posY);
+				imageData.getSelection().setOrigin(posX, posY);
 				highlighting = true;
 			}
-			image.setDimensions(posX, posY);
+			imageData.getSelection().setPoint2(posX, posY);
 		}
 		else
 		{
 			highlighting = false;
 		}
-		
-		$(".highlight").css("border-color", "red");
-	}
-	else
-	{
-		$(".highlight").css("border-color", "green");
 	}
 
-	syncHighlight(image, img);
+	syncHighlight(imageData, img);
 });
 
 $(document).keydown(function(e) 
 {		
-	let E = 69;
-	let R = 82;
+	if (currData === null) return;
+
+	let A = 65;
 	let D = 68;
+	let R = 82;
+	let S = 83;
+
+	let image_saved = getImageData(final_images, currData.url) !== null;
 	
-	if (e.keyCode == E && lastImageData !== null && lastImageData.isComplete())
+	if (e.keyCode == A && !image_saved && currData.selections.length <= MAX_SELECT) // Add
 	{
-		final_images.push(lastImageData);
-		$(".highlight").css("border-color", "green");
+		currData.addSelection();
 	}
-	else if (e.keyCode == R && lastImageData !== null &&
-			 getImage(final_images, lastImageData.url) !== null)
+	if (e.keyCode == S) // Toggle Save
 	{
-		removeImage(final_images, lastImageData.url);
-		$(".highlight").css("border-color", "red");
+		if (!image_saved) // Save
+			final_images.push(currData);
+		else
+			removeImageData(final_images, currData.url);
 	}
-	else if (e.keyCode == D)
+	else if (e.keyCode == R && !image_saved && currData.selections.length > 1) // Remove
+	{
+		currData.removeSelection();
+	}
+	else if (e.keyCode == D) // Download
 	{
 		downloadAll();
 	}
+
+	syncHighlight(currData, currImage)
 });
 
 function syncHighlight(imageData, image)
 {	
 	let left = image.offset().left,
 		top = image.offset().top;
-	
-	let width = imageData.getDimensions().width;
-	let height = imageData.getDimensions().height;
-	
-	$(".highlight").css("left", imageData.getTopLeft().left + left);
-	$(".highlight").css("top", imageData.getTopLeft().top + top);
-	$(".highlight").css("width", width);
-	$(".highlight").css("height", height);
-	
-	lastImageData = imageData;
+
+	for (let i = 0; i < MAX_SELECT; i++)
+	{
+		if (i >= imageData.selections.length)
+		{
+			highlightSetMode(i, NONE);
+		}
+		else
+		{
+			let selection = imageData.selections[i];
+
+			let selLeft = selection.getTopLeft().left + left;
+			let selTop = selection.getTopLeft().top + top;
+			let selWidth = selection.getDimensions().width;
+			let selHeight = selection.getDimensions().height;
+
+			highlightSetSize(i, selLeft, selTop, selWidth, selHeight);
+
+			if (getImageData(final_images, imageData.url) === null) // Image not saved
+			{
+				highlightSetMode(i, SELECTED);
+
+				if (i === imageData.selections.length - 1)
+				{
+					highlightSetMode(i, ACTIVE);
+				}
+			}
+			else
+			{
+				highlightSetMode(i, SAVED);
+			}
+		}
+	}
+
+	currData = imageData;
+	currImage = image;
 }
 
-function getImage(arr, url)
+function highlightSetSize(index, left, top, width, height) // Zero-indexed
+{
+	let highlight = $(".highlight div:nth-child(" + (index + 1) + ")");
+
+	highlight.css("left", left);
+	highlight.css("top", top);
+	highlight.css("width", width);
+	highlight.css("height", height);
+}
+
+function highlightSetMode(index, mode)
+{
+	let highlight = $(".highlight div:nth-child(" + (index + 1) + ")");
+
+	let DISPL_BIT = (mode & 0b100) != 0;
+	let STYLE_BIT = (mode & 0b010) != 0;
+	let COLOR_BIT = (mode & 0b001) != 0;
+
+	highlight.css("display", DISPL_BIT ? "block" : "none");
+	highlight.css("border-style", STYLE_BIT ? "solid" : "dashed");
+	highlight.css("border-color", COLOR_BIT ? "green" : "red");
+}
+
+function getImageData(arr, url)
 {
 	for (let i = 0; i < arr.length; i++)
 	{
-		if (arr[i].url == url)
+		if (arr[i].url === url)
 		{
 			return arr[i];
 		}
@@ -171,13 +237,14 @@ function getImage(arr, url)
 	return null;
 }
 
-function removeImage(arr, url)
+function removeImageData(arr, url)
 {
 	for (let i = 0; i < arr.length; i++)
 	{
-		if (arr[i].url == url)
+		if (arr[i].url === url)
 		{
 			arr.splice(i, 1);
+			return;
 		}
 	}
 }
@@ -186,14 +253,12 @@ function downloadAll()
 {
 	let zip = new JSZip();
 	
-	for (let i = 0; i < final_images.length; i++)
+	final_images.forEach(function(image)
 	{
-		let image = final_images[i];
-		
 		zip.file(image.url.split("/").pop().split("?")[0], urlToPromise(image.url.split("?")[0]), {binary: true});
-	}
+	});
 	
-	zip.file("matrices.txt", final_images.join("\n"));         
+	zip.file("matrices.txt", final_images.join(""));         
 	zip.generateAsync({type:"blob"}).then(function(content)
 	{
 		saveAs(content, "images.zip"); 
@@ -214,9 +279,13 @@ function urlToPromise(url) {
 
 $(document).ready(function()
 {
+	var css = document.createElement("style");
+	css.type = "text/css";
+	css.innerHTML = ".highlight * { position: absolute; border: 1px dashed red; pointer-events: none; z-index: 1000; }";
+	document.body.appendChild(css);
+
 	$("body").append('<div class="highlight"></div>');
-	$(".highlight").css("position", "absolute");
-	$(".highlight").css("border", "1px solid red");
-	$(".highlight").css("pointer-events", "none");
-	$(".highlight").css("z-index", 1000);
+
+	for (let i = 0; i < MAX_SELECT; i++)
+		$(".highlight").append('<div></div>');
 });
